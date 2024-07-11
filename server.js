55fee425f598ff2d41d4557ba7833cce4d16920b
karthikeyanCoder <karthikeyanvijay70@gmail.com>
2024-07-11 12:08:33 +0530

    updated readme file

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import bodyParser from 'body-parser';
-import connectDB from './src/DB/db.js';
+import express from "express";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import connectDB from "./src/DB/db.js";
 import authRoutes from "./src/Routes/authRoutes.js";
-import { verifyToken } from './src/Middleware/authMiddleware.js';
+import { verifyToken } from "./src/Middleware/authMiddleware.js";
 dotenv.config();
-connectDB();
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 app.use(bodyParser.json());
- app.use(verifyToken)
-app.use('/', authRoutes);
+app.use(verifyToken);
+app.use("/", authRoutes);
 
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
+app.listen(PORT, async() => {
+    await connectDB();
     console.log(`Server running on port ${PORT}`);
 });
