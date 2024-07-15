3f377152d7b64dc7dd881d453bc9edd871774f58
karthikeyanCoder <karthikeyanvijay70@gmail.com>
2024-07-15 14:02:42 +0530

    ok

--- src/DB/db.js
+++ src/DB/db.js
@@ -6,6 +6,8 @@ dotenv.config();
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URL);
+    console.log("MongoDB URI:", process.env.MONGO_URL);
+
     console.log("MongoDB Connected Successfully");
   } catch (error) {
     console.error("MongoDB Connection Error", error);
