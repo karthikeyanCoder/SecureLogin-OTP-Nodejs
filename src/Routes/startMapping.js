7f8ad7a9258485d7cd4abf6def7f9e52830c57e9
karthikeyanCoder <karthikeyanvijay70@gmail.com>
2024-09-03 14:09:56 +0530

    ok

--- src/Routes/startMapping.js
+++ src/Routes/startMapping.js
@@ -2,6 +2,6 @@ import { Router } from "express";
 import { saveMappingData } from   "../Controllers/autoController.js"
 
 const router = Router();
-router.post("/start-mapping", saveMappingData);
+router.post("/start-mappings", saveMappingData);
  
 export default router;
